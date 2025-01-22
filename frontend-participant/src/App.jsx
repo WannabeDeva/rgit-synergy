4cd3915159fa8afb347187d0a8b6c05f5bd246ea
Aditya Satish Bhat <127509835+adityabhat09@users.noreply.github.com>
2025-01-22 20:38:13 +0530

    added routes

--- frontend-participant/src/App.jsx
+++ frontend-participant/src/App.jsx
@@ -1,16 +1,38 @@
 
 import './App.css'
-import { Button } from './components/ui/button'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { Button } from './components/ui/button'
+import Create_Teams from './pages/create_teams'
+import Homepage from './pages/homepage'
+import Judgingpage from './pages/judgingpage'
+import Landingpage from './pages/landingpage'
+import Mentorpage from './pages/mentorpage'
+import Workspacepage from './pages/workspace'
+import Navbar from './components/Navbar'
 function App() {
   
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button>Click</Button>
+ 
+
+    <Router>
+      <div>
+        {/* Optional: Add a Button or Navbar here */}
+        <Navbar />
+
+        <Routes>
+          {/* Define routes for the pages */}
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/create-teams" element={<Create_Teams />} />
+          <Route path="/judging" element={<Judgingpage />} />
+          <Route path="/mentor" element={<Mentorpage />} />
+          <Route path="/workspace" element={<Workspacepage />} />
+        </Routes>
+      </div>
+    </Router>
     </>
   )
 }
