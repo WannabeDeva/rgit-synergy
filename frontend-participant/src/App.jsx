1b99bc0cee3c40e740c2042fb7c59eeaac00bf0a
WannabeDeva <devanshukadam2003@gmail.com>
2025-01-22 19:38:53 +0530

    frontend organuzer created

---
+++ frontend-participant/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
