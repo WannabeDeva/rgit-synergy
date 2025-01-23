74686161057305eb17abfba7110a4fa02665c58e
Aditya Satish Bhat <127509835+adityabhat09@users.noreply.github.com>
2025-01-23 13:00:58 +0530

    added team details page

--- frontend-judge/src/App.jsx
+++ frontend-judge/src/App.jsx
@@ -1,9 +1,11 @@
-import TeamDetails from './pages/TeamDetails';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 
 // import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import JudgeDashboard from './pages/JudgeDashboard';
+import TeamDetails from './pages/TeamDetails';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Routes>
         {/* Define a single route for the JudgeDashboard */}
         <Route path="/" element={<JudgeDashboard />} />
+        <Route path="/team/:teamId" element={<TeamDetails />} /> {/* Route to TeamDetails with dynamic teamId */}
         
       </Routes>
     </Router>
