af2082728b7082453b84989016681ed8979e1523
Atharva Yadav <atharvayadav11@gmail.com>
2025-03-19 08:10:29 +0530

    emergency service locator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,13 +23,14 @@ import Notifs from './pages/admin/Notifs';
 // import VideoCallPage from './pages/admin/Videocall';
 // import LiveVideoCall from './pages/LiveVideoCall';
 import ProfileForm from './pages/SignUp/ProfileForm';
-
+import PharmacyLocator from './pages/PharmacyLocator';
 import MedicineOrderPage from './pages/Medicines';
 
 import VideoCallDoctor from './pages/admin/Videocall';
 import VideoCall from './pages/VideoCall';
 import VideoCalling from './pages/admin/VideoCalling';
 
+import NearbyPharmaciesMap from './pages/NearbyPharmaciesMap';
 
 
 function App() {
@@ -38,6 +39,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/video" element={<VideoCall/>} />
+{/* 
+        <Route path="/video" element={<VideoCall/>} /> */}
         <Route path="/medicines" element={<MedicineOrderPage />} />
         {/* <Route path="/video/:roomID/:userID/:userName" element={<VideoCall/>} /> */}
         <Route path="/signup" element={<SignUpForm />} />
@@ -45,6 +48,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/emergency-sos" element={<EmergencySOS />} />
         <Route path="/hospital-locator" element={<HospitalLocator />} />
+        <Route path="/pharmacy-locator" element={<PharmacyLocator />} />
         <Route path="/symptom-checker" element={<SymptomChecker />} />
         <Route path="/first-aid-guide" element={<FirstAidGuide />} />
         <Route path="/medical-records" element={<MedicalRecords />} />
@@ -53,6 +57,7 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/emergency" element={<EmergencyMgmt />} />
         <Route path="/admin/notifs" element={<Notifs />} />
+        <Route path="/nearmeds" element={<NearbyPharmaciesMap />} />
         <Route path="/admin/videocall" element={<VideoCallDoctor />} />
         <Route path="/admin/video-calling" element={<VideoCalling />} />
 
